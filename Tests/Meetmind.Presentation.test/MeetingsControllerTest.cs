095ecc4c18e99794ddb8e1def7f4570aa5e9c1f2
SamEva-dev <sam.fokam@gmail.com>
2025-07-18 22:24:27 +0200

    add controller test

---
+++ Tests/Meetmind.Presentation.test/MeetingsControllerTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using MediatR;
+using Meetmind.Application.Dto;
+using Meetmind.Application.QueryHandles.Mettings;
+using Meetmind.Presentation.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Meetmind.Presentation.test
+{
+    public class MeetingsControllerTest
+    {
+        private readonly MeetingsController _meetingsController;
+
+        private readonly Mock<ISender> _mediatorMock;
+        private readonly Mock<ILogger<MeetingsController>> _loggerMock;
+        public MeetingsControllerTest()
+        {
+            _mediatorMock = new Mock<ISender>();
+            _loggerMock = new Mock<ILogger<MeetingsController>>();
+            _meetingsController = new MeetingsController(_mediatorMock.Object, _loggerMock.Object);
+        }
+        [Fact]
+        public void  GetById_ReturnOk_WhenIdExist()
+        {
+            // Arrange
+            
+            var item = new MeetingDto
+            {
+                Id = Guid.NewGuid(),
+                Title = "Test Meeting",
+                StartUtc = DateTime.UtcNow,
+                EndUtc = DateTime.UtcNow.AddHours(1),
+                State = "Scheduled",
+                TranscriptState = "NotAvailable",
+                SummaryState = "NotAvailable",
+                AudioPath = null,
+                TranscriptPath = null,
+                SummaryPath = null
+            };
+            var result = _mediatorMock.Setup(x => x.Send(It.IsAny<GetMeetingByIdQuery>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(item);
+            var id = Guid.NewGuid();
+            // Act
+            var actionResult = _meetingsController.GetById(id).Result;
+
+            // Assert
+            Assert.NotNull(actionResult);
+            var okResult = actionResult.Result.Should().BeOfType<OkObjectResult>().Subject;
+            okResult.StatusCode.Should().Be(200);
+            okResult.Value.Should().BeEquivalentTo(item);
+        }
+
+        [Fact]
+        public void GetById_ReturnNotFound_WhenIdExist()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            _mediatorMock.Setup(x => x.Send(It.IsAny<GetMeetingByIdQuery>(), It.IsAny<CancellationToken>()))
+                .ThrowsAsync(new KeyNotFoundException());
+            // Act
+            var actionResult = _meetingsController.GetById(id).Result;
+            // Assert
+            Assert.NotNull(actionResult);
+            var notFoundResult = actionResult.Result.Should().BeOfType<NotFoundObjectResult>().Subject;
+            notFoundResult.StatusCode.Should().Be(404);
+            notFoundResult.Value.Should().Be("Meeting not found.");
+        }
+    }
+}
---
+++ Tests/Meetmind.Presentation.test/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace Meetmind.Presentation.test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
